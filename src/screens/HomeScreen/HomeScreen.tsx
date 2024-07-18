4be41d1fe0c601a3fc2ba07f28f9f74c74d678b5
AlexandreJr <juniordias_@live.com>
2024-07-18 16:28:13 -0300

    chore: render items list

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,12 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { FlatList } from 'react-native';
 
-import { Container, Title } from './styles';
+import { Container, Title, Content, Body } from './styles';
+
+export type ItemProps = {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+};
+
+export type DataProps = {
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
+}[];
 
 export const HomeScreen = () => {
+  const [data, setData] = useState<DataProps>([]);
+  const [loading, setLoading] = useState(true);
+
+  const getPosts = () => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const Item = ({ id, userId, title, body }: ItemProps) => (
+    <Content activeOpacity={0.7}>
+      <Title numberOfLines={1}>{title}</Title>
+      <Body numberOfLines={3}>{body}</Body>
+    </Content>
+  );
+
+  console.log(data, 'DATA');
+
   return (
     <Container>
-      <Title>Hello World !</Title>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <Item
+            id={item.id}
+            userId={item.userId}
+            title={item.title}
+            body={item.body}
+          />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
     </Container>
   );
 };
--- src/screens/HomeScreen/styles.ts
+++ src/screens/HomeScreen/styles.ts
@@ -1,13 +1,28 @@
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  flex: 1%;
-  align-items: center;
-  justify-content: center;
+  flex: 1;
   background-color: #f3f3f3;
+  padding-left: 24px;
+  padding-right: 24px;
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
-  color: red;
+  color: #333;
+`;
+
+export const Body = styled.Text`
+  margin-top: 8px;
+  font-size: 16px;
+  color: #333;
+`;
+
+export const Content = styled.TouchableOpacity`
+  width: 100%;
+  height: 144px;
+  background-color: #ffffff;
+  margin-top: 24px;
+  padding: 24px;
+  border-radius: 16px;
 `;
